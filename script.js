0c9c03ddf3a7bf1b81d1ebceea4583db4b531b63
M. Lourdes Linares <lordesbarrera88@gmail.com>
2022-12-28 00:06:11 +0100

    Second animation finished

--- script.js
+++ script.js
@@ -7,8 +7,43 @@ document.getElementById('boton_1').addEventListener(
     'click', (evento) => detener_reanudar1(evento));
 
 function detener_reanudar1(evento){
-    document.querySelector(".anim1_bola1").style.animationPlayState = 
-            document.querySelector(".anim1_bola1").style.animationPlayState === 'running'? 'paused':'running';
-    document.querySelector(".anim1_bola2").style.animationPlayState = 
-            document.querySelector(".anim1_bola2").style.animationPlayState === 'running'? 'paused':'running';
+
+    if (document.querySelector(".anim1_bola1").style.animationPlayState == 'running'){
+        document.querySelector(".anim1_bola1").style.animationPlayState = 
+            'paused';
+        document.querySelector(".anim1_bola2").style.animationPlayState = 
+            'paused';
+        document.getElementById('boton_1').innerHTML = 'Reanudar';
+    }
+    else {
+        document.querySelector(".anim1_bola1").style.animationPlayState = 
+            'running';
+        document.querySelector(".anim1_bola2").style.animationPlayState = 
+            'running';
+        document.getElementById('boton_1').innerHTML = 'Detener';    
+    }
+}
+
+/*Detener/reanudas animacion2 */
+
+document.getElementById('boton_2').addEventListener(
+    'click', (evento) => detener_reanudar2(evento));
+
+function detener_reanudar2(evento){
+
+    if (document.querySelector(".anim2_arco1").style.animationPlayState == 'running'){
+        document.querySelector(".anim2_arco1").style.animationPlayState = 
+            'paused';
+        document.querySelector(".anim2_arco2").style.animationPlayState = 
+            'paused';
+        document.getElementById('boton_2').innerHTML = 'Reanudar';
+    }
+    else {
+        document.querySelector(".anim2_arco1").style.animationPlayState = 
+            'running';
+        document.querySelector(".anim2_arco2").style.animationPlayState = 
+            'running';
+        document.getElementById('boton_2').innerHTML = 'Detener';    
+    }
+    
 }
